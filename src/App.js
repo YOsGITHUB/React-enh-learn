dda4ab4c65aad22f120477fae5907f5df60ed29f
YOsGITHUB <87181417+YOsGITHUB@users.noreply.github.com>
2022-01-04 14:12:24 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState, useCallback, useMemo } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export default function App() {
+  console.log("App");
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  //アロー関数は再生成される。毎回propsが変更されている。関数のmemo化
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  //再レンダリングのたびに計算されるのを避ける。
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+}
